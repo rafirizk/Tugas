18b3cc911bdb651789765b468b8ee870d1480649
rafirizk <rafirizkimaulana@gmail.com>
2020-07-27 14:08:47 +0700

    20200726

---
+++ day5tugas1.js
@@ -0,0 +1,8 @@
+var alphabet = ['a','b','c','d']
+var a = alphabet[0]+alphabet[1]+alphabet[2]
+console.log(a)
+for (let i = 0; i < alphabet.length; i++) {
+    a = alphabet[0+1];
+    
+}
+console.log(a)
--- solveit21.js
+++ solveit21.js
@@ -1,22 +1,44 @@
 let z = ''
-for (let i = 0; i < 6; i++) {
-    for (let j = i; j < 6; j++ ){
-        z += ' ';
-    }for (let j = 0; j < i; j++ ){
-        z += '* '
+for (let i = 0; i < 12; i++) {
+    if (i < 6){
+        for (let j = i; j < 6; j++ ){
+            z += ' ';
+        }
+        for (let j = 0; j < i; j++ ){
+            z += '* '
+        }
+    }
+    else {
+        for (let j = 7; j < i; j++ ){
+            z += ' ';
+        }
+        for (let j = i; j < 12; j++ ){
+            z += '* ';
+        }
     }
     z += '\n'
 }
 
-console.log(z)
+// console.log(z)
 
-for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < i; j++ ){
-        z += ' ';
-    }for (let j = i; j < 6; j++ ){
-        z += '* ';
+var fruit = ["Jeruk", "Apel", "Anggur"];
+var price = [10000, 15000, 20000];
+var stock = [5, 7, 8];
+
+var plusfruit = 'Mangga'
+var plusprice = 10000
+var plusstock = 0
+var minfruit = 1
+
+if (plusstock > 0){
+    fruit.push(plusfruit);
+    price.push(plusprice);
+    stock.push(plusstock);
     }
-    z += '\n'
+else if (minfruit > 0){
+    fruit.slice(minfruit);
 }
+console.log(fruit)
+console.log(price)
+console.log(stock)
 
-console.log(z)
