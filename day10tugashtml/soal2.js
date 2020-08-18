261e93b0c61af5800e8db78a66637efa67b49efb
Rafi Rizki <rafirizkimaulana@gmail.com>
2020-08-18 16:04:51 +0700

    3

--- day10tugashtml/soal2.js
+++ day10tugashtml/soal2.js
@@ -1,32 +1,35 @@
-// totalangka = []
-// do{
-//     angka = prompt("Masukkan Angka")
-//     if (angka >= 0){
-//         totalangka.push(angka)
-//     }
-// } while (angka >= 0)
-// console.log(totalangka)
-// console.log(`Nilai tertinggi : ${a}`)
-// console.log(`Nilai terendah : ${a}`)
-
-let bubbleSort = (arr) => {
-    let len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                var temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
+totalangka = []
+do{
+    angka = prompt("Masukkan Angka")
+    if (angka >= 0){
+        totalangka.push(parseInt(angka))
     }
-    return arr;
-};
+} while (angka >= 0)
+console.log(totalangka)
 
-let highest = (arr)=>{
-    if (arr[0]< arr){
-
+if (!totalangka[1]){
+    var a = totalangka.join('')
+}else if (totalangka[0] > totalangka[1]){
+    if (totalangka[0] > totalangka[2]){
+        var a = totalangka[0]
     }
+} else if (totalangka[1] > totalangka[2]) {
+    var a = totalangka[1]
+} else {
+    var a = totalangka[2]
 }
+console.log(`Nilai tertinggi : ${a}`)
 
-console.log(sort([3,5,7]))
+
+if (!totalangka[1]){
+    var b = totalangka.join('')
+}else if (totalangka[0] < totalangka[1]){
+    if (totalangka[0] < totalangka[2]){
+        var b = totalangka[0]
+    }
+} else if (totalangka[1] < totalangka[2]) {
+    var b = totalangka[1]
+} else {
+    var b = totalangka[2]
+}
+console.log(`Nilai terendah : ${b}`)
